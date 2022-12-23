8304868896a05b983eb51126847904b8f72c96a9
AlexGll <alexnadre.gilles@psl.eu>
2022-12-23 18:21:31 +0100

    determiner les notes

---
+++ Serie_IMDB_test2.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np 
+
+
+def trouver_episodes(tconst_serie):
+    tab_ep = pd.read_csv('data\\title.episode.tsv', sep='\t')
+    episodes = tab_ep[tab_ep['parentTconst']== tconst_serie]
+    #index_ep = episodes.index
+    return episodes
+
+def trier_episodes(episodes):
+    episodes_tries = {}
+    for ep in episodes['tconst'] :
+        index_ep = episodes[episodes['tconst'] == ep].index[0]
+        episodes_tries[ep] = [int(episodes['episodeNumber'][index_ep]),int(episodes['seasonNumber'][index_ep])]
+    return episodes_tries
+
+episodes_tries = trier_episodes(trouver_episodes('tt0050013'))
+
+def nombre(episodes_tries,i):
+    nbr = []
+    for ep in episodes_tries :
+        nbr.append(episodes_tries[ep][i])
+    return len(set(nbr))
+
+def notes(episodes_tries):
+    tab_notes = pd.read_csv('data\\title.ratings.tsv', sep='\t')
+    notes=[[] for _ in range(nombre(episodes_tries,1))]
+    for tconst_ep in episodes_tries :
+        saison = episodes_tries[tconst_ep][1]
+        numero = episodes_tries[tconst_ep][0]
+        index_note = tab_notes[tab_notes['tconst'] == tconst_ep].index[0]
+        print(index_note)
+        print(tab_notes['averageRating'].loc[index_note])
+        note = tab_notes['averageRating'].loc[index_note]
+        notes[saison-1].append(note)
+    return notes
+
+print(notes(episodes_tries))
