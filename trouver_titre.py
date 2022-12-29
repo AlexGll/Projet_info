f195d76db92063c4749824761ca0b68ae112a24e
Karllbl <121167361+Karllbl@users.noreply.github.com>
2022-12-29 14:27:55 +0100

    Update trouver_titre.py

--- trouver_titre.py
+++ trouver_titre.py
@@ -13,6 +13,22 @@ def trouver_titre(titre) :
     infos = serie.loc[index].values('tconst')
     return infos
 
+def trouver_titre(titre) :
+    tmp = pd.read_csv('Desktop/title.basics.tsv', sep='\t', low_memory=False)
+#on selectionne que les séries TV et on enllève les colonnes inutiles  
+    serie = tmp[tmp['titleType'] == 'tvSeries'].drop(['isAdult', 'endYear', 'runtimeMinutes', 'genres'], axis=1)
+#avoir le ou LES index correspondant au titre. Le [0] permet d'accéder à la premièere valeur, il faudra donc fiare une verrification que c'est bien celui demandé par l'utilisateur.
+    index = serie[serie['primaryTitle'] == titre].index
+    infos = serie.loc[index]   
+#récupère toutes les infos sur le titre sélectionné. infos[0] correspont au code tconst
+    if len(index) != 1: 
+       date = input("Quelle est la date du début de la série?\n")
+       for i in range(len(index)):
+        if int(date) == int(infos.iloc[i,4]):
+            infos = infos.loc[infos.index[i]]
+            break
+    return infos[0]
+
 tconst_serie = trouver_titre(titre)
 
 print(tconst_serie)
