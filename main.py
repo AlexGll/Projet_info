dfd000b0309777cba62d171532a4df9f20eceb89
Karllbl <121167361+Karllbl@users.noreply.github.com>
2022-12-29 14:31:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import webbrowser
 titre = input("Quel titre ?\n")
 
 def trouver_titre(titre) :
-    tmp = pd.read_csv('data\\title.basics.tsv', sep='\t', low_memory=False)
+    tmp = pd.read_csv('Desktop/title.basics.tsv', sep='\t', low_memory=False)
 #on selectionne que les séries TV et on enllève les colonnes inutiles  
     serie = tmp[tmp['titleType'] == 'tvSeries'].drop(['isAdult', 'endYear', 'runtimeMinutes', 'genres'], axis=1)
 #avoir le ou LES index correspondant au titre. Le [0] permet d'accéder à la premièere valeur, il faudra donc fiare une verrification que c'est bien celui demandé par l'utilisateur.
-    index = serie[serie['primaryTitle'] == titre].index[0]
+    index = serie[serie['primaryTitle'] == titre].index
+    infos = serie.loc[index]   
 #récupère toutes les infos sur le titre sélectionné. infos[0] correspont au code tconst
-    infos = serie.loc[index]
+    if len(index) != 1: 
+       date = input("Quelle est la date du début de la série?\n")
+       for i in range(len(index)):
+        if int(date) == int(infos.iloc[i,4]):
+            infos = infos.loc[infos.index[i]]
+            break
     return infos[0]
 
 def trouver_episodes(tconst_serie):
