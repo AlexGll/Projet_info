4a72c652e0741904e6290f936ff6a79949842f0e
Karllbl <121167361+Karllbl@users.noreply.github.com>
2022-12-29 14:34:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import webbrowser
 titre = input("Quel titre ?\n")
 
 def trouver_titre(titre) :
-    tmp = pd.read_csv('Desktop/title.basics.tsv', sep='\t', low_memory=False)
+    tmp = pd.read_csv('data\\title.basics.tsv', sep='\t', low_memory=False)
 #on selectionne que les séries TV et on enllève les colonnes inutiles  
     serie = tmp[tmp['titleType'] == 'tvSeries'].drop(['isAdult', 'endYear', 'runtimeMinutes', 'genres'], axis=1)
 #avoir le ou LES index correspondant au titre. Le [0] permet d'accéder à la premièere valeur, il faudra donc fiare une verrification que c'est bien celui demandé par l'utilisateur.
